396ba71d60db9602f960bd7c3edf819606f41b03
husnawan21 <husnawandwi@gmaail.com>
2023-03-04 02:43:41 +0700

    ubah2 git ignore

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
+		formats: ['image/avif', 'image/webp'],
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: '**.vercel.app',
+				hostname: 'assets.vercel.com',
+				port: '',
+				pathname: '/image/upload/**',
 			},
 		],
 	},
